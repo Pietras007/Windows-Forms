3a00beee3ba4f4c78d90f05c0a04901469ad0d44
Pietras007 <piotr.samborski007@gmail.com>
2019-05-08 22:48:31 +0200

    Laboratory task

---
+++ Picture Filter (Task 3)/Picture Filter/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace Picture_Filter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.showColorCheckBox = new System.Windows.Forms.CheckBox();
+            this.apply_FilterButton = new System.Windows.Forms.Button();
+            this.sharpenButton = new System.Windows.Forms.RadioButton();
+            this.negativeButton = new System.Windows.Forms.RadioButton();
+            this.grayscaleButton = new System.Windows.Forms.RadioButton();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.groupBox1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.showColorCheckBox);
+            this.groupBox1.Controls.Add(this.apply_FilterButton);
+            this.groupBox1.Controls.Add(this.sharpenButton);
+            this.groupBox1.Controls.Add(this.negativeButton);
+            this.groupBox1.Controls.Add(this.grayscaleButton);
+            this.groupBox1.Location = new System.Drawing.Point(619, 27);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(160, 509);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Filters";
+            // 
+            // showColorCheckBox
+            // 
+            this.showColorCheckBox.AutoSize = true;
+            this.showColorCheckBox.Location = new System.Drawing.Point(7, 130);
+            this.showColorCheckBox.Name = "showColorCheckBox";
+            this.showColorCheckBox.Size = new System.Drawing.Size(77, 17);
+            this.showColorCheckBox.TabIndex = 4;
+            this.showColorCheckBox.Text = "ShowColor";
+            this.showColorCheckBox.UseVisualStyleBackColor = true;
+            this.showColorCheckBox.CheckedChanged += new System.EventHandler(this.showColorCheckBox_CheckedChanged);
+            // 
+            // apply_FilterButton
+            // 
+            this.apply_FilterButton.Location = new System.Drawing.Point(7, 91);
+            this.apply_FilterButton.Name = "apply_FilterButton";
+            this.apply_FilterButton.Size = new System.Drawing.Size(140, 32);
+            this.apply_FilterButton.TabIndex = 3;
+            this.apply_FilterButton.Text = "Apply filter";
+            this.apply_FilterButton.UseVisualStyleBackColor = true;
+            this.apply_FilterButton.Click += new System.EventHandler(this.apply_FilterButton_Click);
+            // 
+            // sharpenButton
+            // 
+            this.sharpenButton.AutoSize = true;
+            this.sharpenButton.Location = new System.Drawing.Point(7, 68);
+            this.sharpenButton.Name = "sharpenButton";
+            this.sharpenButton.Size = new System.Drawing.Size(65, 17);
+            this.sharpenButton.TabIndex = 2;
+            this.sharpenButton.Text = "Sharpen";
+            this.sharpenButton.UseVisualStyleBackColor = true;
+            // 
+            // negativeButton
+            // 
+            this.negativeButton.AutoSize = true;
+            this.negativeButton.Location = new System.Drawing.Point(7, 44);
+            this.negativeButton.Name = "negativeButton";
+            this.negativeButton.Size = new System.Drawing.Size(68, 17);
+            this.negativeButton.TabIndex = 1;
+            this.negativeButton.Text = "Negative";
+            this.negativeButton.UseVisualStyleBackColor = true;
+            // 
+            // grayscaleButton
+            // 
+            this.grayscaleButton.AutoSize = true;
+            this.grayscaleButton.Checked = true;
+            this.grayscaleButton.Location = new System.Drawing.Point(7, 20);
+            this.grayscaleButton.Name = "grayscaleButton";
+            this.grayscaleButton.Size = new System.Drawing.Size(72, 17);
+            this.grayscaleButton.TabIndex = 0;
+            this.grayscaleButton.TabStop = true;
+            this.grayscaleButton.Text = "Grayscale";
+            this.grayscaleButton.UseVisualStyleBackColor = true;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 539);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(784, 22);
+            this.statusStrip1.TabIndex = 1;
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(667, 17);
+            this.toolStripStatusLabel1.Spring = true;
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.BackColor = System.Drawing.Color.Lime;
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(112, 22);
+            this.openToolStripMenuItem.Text = "Open...";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(20, 20);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(40, 40);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(595, 503);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 27);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(601, 509);
+            this.panel1.TabIndex = 6;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(600, 400);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button apply_FilterButton;
+        private System.Windows.Forms.RadioButton sharpenButton;
+        private System.Windows.Forms.RadioButton negativeButton;
+        private System.Windows.Forms.RadioButton grayscaleButton;
+        private System.Windows.Forms.CheckBox showColorCheckBox;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Picture Filter (Task 3)/Picture Filter/Form1.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Picture_Filter
+{
+    public partial class Form1 : Form
+    {
+        Bitmap picture;
+        Bitmap workingPicture;
+        Bitmap workingPicture2;
+        public Form1()
+        {
+            InitializeComponent();
+            toolStripProgressBar1.Visible = false;
+            pictureBox2.Visible = false;
+            pictureBox1.Controls.Add(pictureBox2);
+            pictureBox2.Left = 10;
+            pictureBox2.Top = 10;
+            backgroundWorker1.WorkerReportsProgress = true;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "JPG Image (.jpg)|*.jpg|Bitmap Image (.bmp)|*.bmp|Gif Image (.gif)|*.gif";
+            openFileDialog.Title = "Select a Image File";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                LoadImage(openFileDialog.FileName);
+            }
+        }
+
+        private void LoadImage(string path)
+        {
+            toolStripStatusLabel1.Text = path.ToString();
+            picture = new Bitmap(path);
+            pictureBox1.Image = picture;
+        }
+
+        private void showColorCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox check = sender as CheckBox;
+            if (check.Checked == true)
+            {
+                pictureBox2.Visible = true;
+            }
+            else
+            {
+                pictureBox2.Visible = false;
+            }
+
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (picture != null)
+            {
+                Color color = picture.GetPixel(e.X, e.Y);
+                pictureBox2.BackColor = color;
+            }
+            else
+            {
+                pictureBox2.BackColor = Color.White;
+            }
+
+        }
+
+        private void apply_FilterButton_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy != true)
+            {
+                toolStripProgressBar1.Visible = true;
+                workingPicture = (Bitmap)picture.Clone();
+                workingPicture2 = (Bitmap)picture.Clone();
+
+                backgroundWorker1.RunWorkerAsync();
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int which_effect = 0;
+            if (grayscaleButton.Checked == true)
+            {
+                which_effect = 0;
+            }
+            else if (negativeButton.Checked == true)
+            {
+                which_effect = 1;
+            }
+            else if (sharpenButton.Checked == true)
+            {
+                which_effect = 2;
+            }
+
+            if (picture != null)
+            {
+                backgroundWorker1.ReportProgress(0);
+                int width = workingPicture.Width;
+                int height = workingPicture.Height;
+                for (int i = 0; i < width; i++)
+                {
+                    for (int j = 0; j < height; j++)
+                    {
+                        switch (which_effect)
+                        {
+                            case 0:
+                                Grayscale(i, j, width, height);
+                                break;
+                            case 1:
+                                Negative(i, j, width, height);
+                                break;
+                            case 2:
+                                Sharpen(i, j, width, height);
+                                break;
+                        }
+                    }
+                    backgroundWorker1.ReportProgress((int)((double)(i + 1) / width * 100) % 101);
+                }
+                backgroundWorker1.ReportProgress(100);
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            toolStripProgressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            pictureBox1.Image = workingPicture;
+            picture = workingPicture;
+            pictureBox1.Image = picture;
+            toolStripProgressBar1.Visible = false;
+        }
+
+        private void Grayscale(int i, int j, int width, int height)
+        {
+            Color color = workingPicture.GetPixel(i, j);
+            int Y = ((int)(color.R * 0.299 + color.G * 0.587 + color.B * 0.114)) % 256;
+            Color endColor = Color.FromArgb(Y, Y, Y);
+            workingPicture.SetPixel(i, j, endColor);
+        }
+
+        private void Negative(int i, int j, int width, int height)
+        {
+            Color color = workingPicture.GetPixel(i, j);
+            Color endColor = Color.FromArgb(255 - color.R, 255 - color.G, 255 - color.B);
+            workingPicture.SetPixel(i, j, endColor);
+        }
+
+        private void Sharpen(int i, int j, int width, int height)
+        {
+            int red = 0;
+            int green = 0;
+            int blue = 0;
+            for (int x = i - 1; x <= i + 1; x++)
+            {
+                for (int y = j - 1; y <= j + 1; y++)
+                {
+                    Color c = workingPicture2.GetPixel(Clamp(x, 0, width - 1), Clamp(y, 0, height - 1));
+                    if (x != i || y != j)
+                    {
+                        red -= c.R;
+                        green -= c.G;
+                        blue -= c.B;
+                    }
+                    else
+                    {
+                        red += 9 * c.R;
+                        green += 9 * c.G;
+                        blue += 9 * c.B;
+                    }
+                }
+            }
+            red = Clamp(red, 0, 255);
+            green = Clamp(green, 0, 255);
+            blue = Clamp(blue, 0, 255);
+
+            Color endColor = Color.FromArgb(red, green, blue);
+            workingPicture.SetPixel(i, j, endColor);
+        }
+
+        public int Clamp(int val, int min, int max)
+        {
+            if (val < min)
+            {
+                val = min;
+            }
+            else if (val > max)
+            {
+                val = max;
+            }
+
+            return val;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (backgroundWorker1.IsBusy == true)
+            {
+                MessageBox.Show("Working...", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                e.Cancel = true;
+            }
+        }
+    }
+}
