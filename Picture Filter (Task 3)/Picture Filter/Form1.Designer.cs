387dc815becd16d9b8b686c94b394863e408b3b6
Pietras007 <piotr.samborski007@gmail.com>
2019-05-09 00:23:12 +0200

    Laboratory task

--- Picture Filter (Task 3)/Picture Filter/Form1.Designer.cs
+++ Picture Filter (Task 3)/Picture Filter/Form1.Designer.cs
@@ -188,6 +188,9 @@
             // 
             // panel1
             // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.AutoScroll = true;
             this.panel1.Controls.Add(this.pictureBox2);
             this.panel1.Controls.Add(this.pictureBox1);
